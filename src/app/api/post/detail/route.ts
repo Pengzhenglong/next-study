6246ea7629b6386c9b78f140f2b95854323046b2
H.M.E <1316126217@qq.com>
2026-01-07 17:16:18 +0800

    feat: Add MongoDB integration to fetch post details by ID.

--- src/app/api/post/detail/route.ts
+++ src/app/api/post/detail/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest } from 'next/server';
 import { withApiHandler } from '@/utils/withApiHandler';
 import { success, error } from '@/utils/apiResponse';
-
+import clientPromise from '@/lib/mongodb';
+import { DB_NAME } from '@/config/constants';
 
 export const GET = withApiHandler(async (req: NextRequest) => {
     const { searchParams } = new URL(req.url);
@@ -12,4 +13,12 @@ export const GET = withApiHandler(async (req: NextRequest) => {
             {status: 400}
         );
     }
+
+    const client = await clientPromise;
+    const db = client.db(DB_NAME);
+    const collection = db.collection('posts');
+    const post = await collection.findOne({ id });
+    return Response.json(success(post), {
+        status: 200
+    });
 })
